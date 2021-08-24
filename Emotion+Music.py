7123b6196faa6b327d74503faac87f2c1d8a9467
Zaeem Ghauri <zaeemghauri@gmail.com>
2021-08-24 15:50:31 -0400

    Combined GUI+openCV

--- Emotion+Music.py
+++ Emotion+Music.py
@@ -1,23 +1,75 @@
+# IMPORTING NECESSARY LIBRARIES
 import tkinter as tk
-from tkinter import *
-from EmotionRecognition import Emotion
+from PIL import Image, ImageTk
+import cv2
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array
 
-track = Emotion()
-
-class Application(Frame):
+class Application:
     def __init__(self):
-        tk.Frame.__init__(self)
-        self.pack()
-        self.master.title("Emotion=Music")
+        # MODELS AND HAAR CASCADE FOR FACIAL DETECTION
+        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        self.model = load_model('EmotionDetectionModel.h5')
+        self.emotions = ['Angry', 'Happy', 'Neutral', 'Sad', 'Surprise']
+        self.capture = cv2.VideoCapture(0)
 
-        self.start = Button(self)
-        self.start["text"] = "Start video capture"
-        self.start["command"] = track.startCapture
-        self.start.pack({"side": "left"})
+        #GUI
+        self.root = tk.Tk()
+        self.root.title("Emotion+Music")
+        self.panel = tk.Label(self.root)
+        self.panel.pack()
 
-        self.stop = Button(self)
-        self.stop["text"] = "Stop video capture"
-        self.start["command"] = track.capture.release()
-        self.stop.pack({"side": "right"})
+        #BUTTONS
+        stop = tk.Button(self.root, text="Close Program", command=self.stopCapture)
+        stop.pack(fill="both", expand=True, padx=10, pady=10)
 
-Application().mainloop()
+        self.startCapture() # poll function to display video
+
+    def startCapture(self):
+        # MAKE SURE FRAMES ARE BEING READ PROPERLY
+        read, frame = self.capture.read()
+        if (not read):
+            print('Frames have NOT been read properly')
+
+        # FACIAL DETECTION
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face_detected = self.face_cascade.detectMultiScale(gray_img, 1.3, 5)  # detects and 'draws' rectangle around face
+
+        for (x, y, w, h) in face_detected:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  # actually draws the rectangle
+            roi_gray = gray_img[y:y + h, x:x + w]  # region of interest (focus on the face)
+            roi_gray = cv2.resize(roi_gray, (48, 48),
+                                  interpolation=cv2.INTER_AREA)  # resize image for better processing
+
+            # EMOTION DETECTION
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype(
+                    'float') / 255.0  # convert pixel values from 0-255 to 0-1 to make processing easier
+                roi = img_to_array(roi)  # convert to array to pass onto model
+                roi = np.expand_dims(roi, axis=0)
+
+                prediction = self.model.predict(roi)[0]
+                emotion_prediction = self.emotions[prediction.argmax()]
+
+                cv2.putText(frame, emotion_prediction, (int(x), int(y)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+
+        # DISPLAY VIDEO FOOTAGE
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)  # convert colors BGR -> RGB
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        self.panel.imgtk = imgtk
+        self.panel.config(image=imgtk)  # show the image
+
+        cv2.waitKey(0)
+
+        self.root.after(10, self.startCapture)  # repeat this function every 10 milliseconds
+
+    def stopCapture(self):
+        print("Closing program...")
+        self.root.destroy()
+        self.capture.release()
+
+app = Application()
+app.root.mainloop()
--- EmotionRecognition.py
+++ EmotionRecognition.py
@@ -1,3 +1,4 @@
+# NO LONGER NEEDED (STAND ALONE PROGRAM JUST FOR EMO RECOGNITION)
 # IMPORTING NECESSARY LIBRARIES
 import cv2
 import numpy as np
@@ -5,52 +6,48 @@ from keras.models import load_model
 from keras.preprocessing import image
 from keras.preprocessing.image import img_to_array
 
-class Emotion:
-    def __init__(self):
-        # MODELS AND HAAR CASCADE FOR FACIAL DETECTION
-        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        self.model = load_model('EmotionDetectionModel.h5')
-        self.emotions = ['Angry', 'Happy', 'Neutral', 'Sad', 'Surprise']
+# MODELS AND HAAR CASCADE FOR FACIAL DETECTION
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+model = load_model('EmotionDetectionModel.h5')
 
-        self.capture = cv2.VideoCapture(0)
+emotions = ['Angry', 'Happy', 'Neutral', 'Sad', 'Surprise']
 
-    def startCapture(self):
-        self.capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)  # stream webcam footage
 
-        while True:
-            # MAKE SURE FRAMES ARE BEING READ PROPERLY
-            read, frame = self.capture.read()
-            if (not read):
-                print('Frames have NOT been read properly')
-                break
+while True:
+    labels = []
 
-            # FACIAL DETECTION
-            gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            face_detected = self.face_cascade.detectMultiScale(gray_img, 1.3,
-                                                               5)  # detects and 'draws' rectangle around face
+    # MAKE SURE FRAMES ARE BEING READ PROPERLY
+    read, frame = capture.read()
+    if (not read):
+        print('Frames have NOT been read properly')
+        break
 
-            for (x, y, w, h) in face_detected:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  # actually draws the rectangle
-                roi_gray = gray_img[y:y + h, x:x + w]  # region of interest (focus on the face)
-                roi_gray = cv2.resize(roi_gray, (48, 48),
-                                      interpolation=cv2.INTER_AREA)  # resize image for better processing
+    # FACIAL DETECTION
+    gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_detected = face_cascade.detectMultiScale(gray_img, 1.3, 5)  # detects and 'draws' rectangle around face
 
-                # EMOTION DETECTION
-                if np.sum([roi_gray]) != 0:
-                    roi = roi_gray.astype(
-                        'float') / 255.0  # convert pixel values from 0-255 to 0-1 to make processing easier
-                    roi = img_to_array(roi)  # convert to array to pass onto model
-                    roi = np.expand_dims(roi, axis=0)
+    for (x,y,w,h) in face_detected:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)                   # actually draws the rectangle
+        roi_gray = gray_img[y:y+h, x:x+w]                                       # region of interest (focus on the face)
+        roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA) # resize image for better processing
 
-                    prediction = self.model.predict(roi)[0]
-                    emotion_prediction = self.emotions[prediction.argmax()]
+        # EMOTION DETECTION
+        if np.sum([roi_gray]) != 0:
+            roi = roi_gray.astype('float') / 255.0  # convert pixel values from 0-255 to 0-1 to make processing easier
+            roi = img_to_array(roi)                 # convert to array to pass onto model
+            roi = np.expand_dims(roi, axis=0)
 
-                    cv2.putText(frame, emotion_prediction, (int(x), int(y)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+            prediction = model.predict(roi)[0]
+            emotion_prediction = emotions[prediction.argmax()]
 
-            # DISPLAY VIDEO FOOTAGE
-            cv2.imshow('Emotion Detector', frame)
+            cv2.putText(frame, emotion_prediction, (int(x), int(y)), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
 
-            # emergency break
-            if cv2.waitKey(20) & 0xFF == ord(' '):
-                break
+    # DISPLAY VIDEO FOOTAGE
+    cv2.imshow('Emotion Detector', frame)
 
+    if cv2.waitKey(20) & 0xFF == ord(' '):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
